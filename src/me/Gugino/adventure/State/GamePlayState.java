40e3987dcf4f83a7190a755d5cbbd95507508c01
Ephyxia <limitedxstudios@gmail.com>
2013-08-05 23:49:08 -0400

    Resized sprites and background

--- src/me/Gugino/adventure/State/GamePlayState.java
+++ src/me/Gugino/adventure/State/GamePlayState.java
@@ -3,12 +3,16 @@ package me.Gugino.adventure.State;
 import me.Gugino.adventure.Entities.EntityPlayer;
 
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
 
 public class GamePlayState extends GameState  {
 
 	private int mx;
 	private int my;
+	
+	private Image bg;
 	
 	public static EntityPlayer player;
 
@@ -20,7 +24,11 @@ public class GamePlayState extends GameState  {
 	public void Init() {
 		player = new EntityPlayer(480, 480);
 		
-		
+		try {
+			bg = new Image("res/images/strips/testBG.png");
+		} catch (SlickException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -33,6 +41,7 @@ public class GamePlayState extends GameState  {
 
 	@Override
 	public void Render(Graphics g) {
+		bg.draw(0,-24);
 		player.render(g);
 	}
 }
